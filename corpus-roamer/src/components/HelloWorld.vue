f06956a318606f691728fc7057d083e71d4df785
xdqc <qichen.d@outlook.com>
2019-01-18 04:57:04 +1300

    word scroller

--- corpus-roamer/src/components/HelloWorld.vue
+++ corpus-roamer/src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hello">
     <!-- <h3>{{msg}}</h3> -->
-    <input type="number" v-bind="cursor_idx" @change="changeCursor()">
+    <p>Press <code>J</code> and <code>K</code> to go up and down.</p>
+    <input type="number" v-model="cursor" :max="WL_size" min="0" step="10">
     <ul>
-      <li v-for="(word,i) in word_list" :key="i" > 
-        <!-- v-show="i < cursor_idx+10 && i>=cursor_idx" -->
-        <span>{{ word }}</span>
+      <li v-for="(word,i) in word_list" :key="i">
+        <a :href="'https://www.etymonline.com/word/'+word" target="_blank">
+          <span :class="{ 'current-cursor': (cursor<10 ? cursor==i : i==10)}">{{ word }}</span>
+        </a>
       </li>
     </ul>
   </div>
@@ -21,24 +23,38 @@ export default {
   },
   data() {
     return {
-      cursor_idx: Number
+      WL_size: 61420,
+      cursor: 4700,
+      speakMsg: new SpeechSynthesisUtterance()
     };
   },
   computed: {
-    word_list : function(){
-      return WL.slice(this.cursor_idx, this.cursor_idx+10);
-    }
+    word_list: function() {
+      const start = this.cursor >10 ? parseInt(this.cursor)-10 : 0;
+      return WL.slice(start, parseInt(this.cursor) + 10);
+    },
   },
   mounted() {
-    this.cursor_idx = 0;
     window.addEventListener("keypress", e => {
-      // console.log(e.keyCode);
-      if (e.keyCode === 39 && this.cursor_idx > 0) {
-        this.cursor_idx--;
-      } else if (e.keyCode === 46) {
-        this.cursor_idx++;
+      if (e.keyCode === 106 && this.cursor > 0) {
+        this.cursor--;
+        this.speakWord(this.cursor);
+      } else if (e.keyCode === 107 && this.cursor < this.WL_size) {
+        this.cursor++;
+        this.speakWord(this.cursor);
+      } else if (e.keyCode === 113) {
+        this.speakWord(this.cursor);
+      } else {
+        speechSynthesis.cancel();
       }
     });
+  },
+  methods: {
+    speakWord(index) {
+      this.speakMsg.text = WL[index];
+      this.speakMsg.voice = speechSynthesis.getVoices()[9];
+      speechSynthesis.speak(this.speakMsg);
+    }
   }
 };
 </script>
@@ -54,9 +70,15 @@ ul {
 }
 li {
   /* display: inline-block; */
-  margin: 0 10px;
+  margin: 10px 10px;
 }
 a {
-  color: #42b983;
+  color: #2c3e5096;
+  text-decoration:none
 }
+.current-cursor {
+  color: #42b983;
+  /* color:#f7786b; */
+  font-size: x-large;
+};
 </style>
