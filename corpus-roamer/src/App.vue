c1306aeb7069669494ef4ae3a83214296edfd204
Samuel Ding <samuel.ding.nz@gmail.com>
2020-06-27 21:04:12 +0200

    add webapp

---
+++ corpus-roamer/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Roamer msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Roamer from "./components/Roamer.vue";
+
+export default {
+  name: "app",
+  components: {
+    Roamer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
